ab8dc74529c986ca6965876bcfb29497d06025dc
alexsandroferrao <alexsandroferrao0203@gmail.com>
2022-01-23 23:13:39 -0300

    Adicionando arquivos do tsoa configurando swagger

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,17 +1,32 @@
-import { createExpressServer } from "routing-controllers";
+import { useExpressServer } from "routing-controllers";
 import { MoneyController } from "./controllers/moneyController";
-import 'reflect-metadata'
+import express from "express";
+import "reflect-metadata";
+import bodyParser from "body-parser";
+import { RegisterRoutes } from "./routes";
+import * as swaggerUi from "swagger-ui-express";
 
 const porta = 3000;
+const app = express();
 
-export const app = createExpressServer({
-    controllers:[MoneyController],
-})
+useExpressServer(app, {
+  controllers: [MoneyController],
+});
 
+app.use(bodyParser.json());
+RegisterRoutes(app);
 
-if(process.env.NODE_ENV !== 'test'){
-    app.listen(process.env.PORT || porta, ()=>{
-        console.log('Aplicacao rodando na porta ' + porta)
-    })
+try{
+    const swaggerDocument = require('../../swagger.json');
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+}catch(err){
+    console.error('Não foi possível ler o swagger.json')
 }
 
+if (process.env.NODE_ENV !== "test") {
+  app.listen(process.env.PORT || porta, () => {
+    console.log("Aplicacao rodando na porta " + porta);
+  });
+}
+
+export {app}
